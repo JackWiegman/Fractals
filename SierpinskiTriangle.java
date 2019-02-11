78aff0f96ee444dced5fbcf89045b38491ab8bf2
Jack Wiegman <jawiegman@gmail.com>
2019-02-11 08:28:32 -0700

    Draw background and background triangle

--- SierpinskiTriangle.java
+++ SierpinskiTriangle.java
@@ -3,6 +3,9 @@ import java.awt.Color;
 
 public class SierpinskiTriangle {
 
+	private static Point2D.Double min, max;
+	private static Color background = StdDraw.GRAY;
+
 	public static void main(String[] args) {
 		setup();
 		loop();
@@ -14,13 +17,29 @@ public class SierpinskiTriangle {
 
 		StdDraw.setXscale(min.x, max.x);
 		StdDraw.setYscale(min.y, max.y);
-		StdDraw.enableDoubleBuffering();
-
-		h = new H(max.x / 2.0, max.y / 2.0, 10, stroke);
+		
 	}
 
 	public static void loop() {
-		
+		int n = 6;
+		StdDraw.clear(background);
+
+		Triangle backgroundTriangle = new Triangle();
+
+		Point2D.Double backTriTop = new Point2D.Double(max.x / 2, max.y);
+		Point2D.Double backTriLeft = new Point2D.Double(0, 0);
+		Point2D.Double backTriRight = new Point2D.Double(max.x, 0);
+		backgroundTriangle.drawTriangle(backTriTop, backTriLeft, backTriRight, StdDraw.BLACK);
+
+		drawTree(n);
+
+		StdDraw.show();
+	}
+
+	public static void drawTree(int n) {
+		// if (n < 1) {
+		// 	drawTriangle(new Point2D.Double(back))
+		// }
 	}
 
 }
--- Triangle.java
+++ Triangle.java
@@ -5,10 +5,29 @@ public class Triangle {
 
 	private Point2D.Double center;
 	private double leg;
+	private Color stroke;
 
-	public Triangle(double centerX, double centerY, double leg) {
-		center = new Point2D.Double(centerX, centerY);
-		this.leg = leg;
+	public Triangle() {
+		// center = new Point2D.Double(centerX, centerY);
+		// this.leg = leg;
+	}
+
+	public void drawTriangle(Point2D.Double point1, Point2D.Double point2,
+							Point2D.Double point3, Color fill) {
+		StdDraw.setPenRadius(0.01);
+		StdDraw.setPenColor(fill);
+
+		// StdDraw.line(point1.x, point1.y, point2.x, point2.y);
+		// StdDraw.line(point3.x, point3.y, point2.x, point2.y);
+		// StdDraw.line(point1.x, point1.y, point3.x, point3.y);
+
+		double[] xPoints = {point1.x, point2.x, point3.x};
+		double[] yPoints = {point1.y, point2.y, point3.y};
+
+		StdDraw.filledPolygon(xPoints, yPoints);
+
+		// System.out.println(StdDraw.getPenRadius());
+
 	}
 
 }
